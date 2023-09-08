767f922eedb8e812c1acc04f2ce2f36d5c36115b
EmileKost <emile.kost@hva.nl>
2023-09-08 15:18:19 +0200

    feat: Fix useEffect

--- src/app/faq/page.tsx
+++ src/app/faq/page.tsx
@@ -9,7 +9,7 @@ import { Menu } from '@/components/Menu';
 
 const Faq = () => {
 
-    const [ctaText, setCtaText] = useState(window.innerWidth < 670 ? 'Leave you alone' : 'Stop bothering my co-workers');
+    const [ctaText, setCtaText] = useState('Stop bothering my co-workers');
 
     useEffect(() => {
         function getCorrectContent() {
